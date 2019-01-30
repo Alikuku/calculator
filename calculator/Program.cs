081e5892c85d285ce6e127d5a48a0e95580f3d3f
danielappelgren <dd_a_boy@hotmail.com>
2019-01-30 11:43:31 +0100

    update code to fit better

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,31 +6,29 @@ using System.Threading.Tasks;
 
 namespace calculator
 {
+    public class Calculator
+    {
 
-   
-        
-            public class Calculator
+        public double Add(double a, double b)
         {
-            public double Add(double a, double b)
-            {
-                return a + b;
-            }
+            return a + b;
+        }
 
-            public double Subtract(double a, double b)
-            {
-                return a - b;
-            }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
 
-            public double Multiply(double a, double b)
-            {
-                return a * b;
-            }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
 
-            public double Power(double a, double b)
-            {
-                return Math.Pow(a, b);
-            }
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
         }
     }
+}
     
 

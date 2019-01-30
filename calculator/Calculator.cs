da4685d2f5efad37dd3dff75892b53a3ee408e01
Florent <au523923@uni.au.dk>
2019-01-30 12:10:48 +0100

    Changed class name to upper case. Changed file names (cs files), to correspond to class names.

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calculator
+namespace Calculator
 {
     public class Calculator
     {
--- unitTestCalculator/unitTestCalculator.cs
+++ unitTestCalculator/unitTestCalculator.cs
@@ -14,7 +14,7 @@ namespace unitTestCalculator
         [Test] 
         public void add2plus4()
         {
-            var uut = new calculator.Calculator();
+            var uut = new Calculator.Calculator();
             Assert.That(uut.Add(2, 4), Is.EqualTo(6));
 
         }
@@ -22,21 +22,21 @@ namespace unitTestCalculator
         [Test]
         public void sub2minus2()
         {
-            var uut = new calculator.Calculator();
+            var uut = new Calculator.Calculator();
             Assert.That(uut.Subtract(2, 2), Is.EqualTo(0));
         }
 
         [Test]
         public void gang2gang2()
         {
-            var uut = new calculator.Calculator();
+            var uut = new Calculator.Calculator();
             Assert.That(uut.Multiply(2, 2), Is.EqualTo(4));
         }
 
         [Test]
         public void pow2pow2()
         {
-            var uut = new calculator.Calculator();
+            var uut = new Calculator.Calculator();
             Assert.That(uut.Power(2, 2), Is.EqualTo(4));
         }
     }

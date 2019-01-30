8a9ce16fa5f9547330517b732e9a2dec94dc35f2
ali <alielkaatib@hotmai.com>
2019-01-30 11:12:19 +0100

    Add project files.

---
+++ unitTestCalculator/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace unitTestCalculator
+{
+    [TestFixture]
+    public class unitTestCalculator
+    {
+        [Test] 
+        public void add2plus2()
+        {
+            var uut = new calculator.Calculator();
+            Assert.That(uut.Add(2, 2), Is.EqualTo(4));
+
+        }
+
+        [Test]
+        public void sub2minus2()
+        {
+            var uut = new calculator.Calculator();
+            Assert.That(uut.Subtract(2, 2), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void gang2gang2()
+        {
+            var uut = new calculator.Calculator();
+            Assert.That(uut.Multiply(2, 2), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void pow2pow2()
+        {
+            var uut = new calculator.Calculator();
+            Assert.That(uut.Power(2, 2), Is.EqualTo(4));
+        }
+    }
+}
